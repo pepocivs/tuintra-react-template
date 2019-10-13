20a8f903f5a1428c4d5ceda6d4c10af2c4a5777b
Jorge Martín <tcbpepo@gmail.com>
2019-10-13 15:55:08 +0800

    chore: increase number of birthdayDays

--- src/redux/people/api.js
+++ src/redux/people/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import clubInfo from "helpers/clubInfo";
 
 const fetchPeople = (params) => {
-  const queryParams = (params === 'birthdays') ? '?nextBirthdays=3' : '';
+  const queryParams = (params === 'birthdays') ? '?nextBirthdays=5' : '';
   return axios.get(`${clubInfo.apiHost}/${clubInfo.subdomain}/people${queryParams}`);
 }
 
