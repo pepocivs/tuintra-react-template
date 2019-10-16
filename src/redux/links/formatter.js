17da669cc112afd9614c13a53c6a64cf9122cab4
Jorge Martín <tcbpepo@gmail.com>
2019-10-16 18:41:55 +0800

    chore: sort links

--- src/redux/links/formatter.js
+++ src/redux/links/formatter.js
@@ -1,7 +1,9 @@
 export default (data) => {
   if (!data[0]) return [];
+  const sortList = [1, 8, 2, 3, 4, 7];
+  const links = data.filter(link => (link.categoryId !== 5 && link.categoryId !== 6) && link.links.length > 0);  
   return {
-    links: data.filter(link => (link.categoryId !== 5 && link.categoryId !== 6)),
+    links: links.sort((a,b) => (sortList.indexOf(a.categoryId) > sortList.indexOf(b.categoryId) ? 1 : -1)),
     footerLinks: data.filter(link => (link.categoryId === 5 || link.categoryId === 6)),
   };
 }
