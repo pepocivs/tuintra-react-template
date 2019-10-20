90ebb02752434f01cf00f80c5eb5e7e7ed88ed6e
Jorge Martín <tcbpepo@gmail.com>
2019-10-20 21:48:29 +0200

    chore: sort directors

--- src/helpers/sortBy.js
+++ src/helpers/sortBy.js
@@ -2,16 +2,25 @@ const sortBy = (array, key, sort = 1) => {
   return array.sort((a, b) => (a[key] > b[key]) ? (1 * sort) : (a[key] < b[key]) ? (-1 * sort) : 0);
 }
 
+const arrayIndexContains = (array, value) => {
+  let findedIndex = -1;
+  // eslint-disable-next-line array-callback-return
+  array.map((element, index) => {
+    if (value.includes(element)) findedIndex = index;
+  })  
+  return findedIndex;
+}
+
 const sortByArray = (array, key, arraySort) => {
   return array.sort((a,b) => 
-    (arraySort.indexOf(a[key]) > arraySort.indexOf(b[key]) 
+    (arrayIndexContains(arraySort, a[key]) > arrayIndexContains(arraySort, b[key]))
       ? 1 
-      : (arraySort.indexOf(a[key]) < arraySort.indexOf(b[key]) 
+      : (arrayIndexContains(arraySort, a[key]) < arrayIndexContains(arraySort, b[key]))
         ? -1 
         : 0
-      )
-    )
   );
-}
+};
+
+
 
 export { sortBy, sortByArray };
--- src/redux/people/selectors.js
+++ src/redux/people/selectors.js
@@ -1,8 +1,15 @@
 import { createSelector } from "reselect";
 import { get, isEmpty } from "lodash";
+import { sortByArray } from "../../helpers/sortBy";
 
 const getDirectors = (people) => {
-  return (people.length) ? people.filter(person => person.directorData) : people;
+  if (Object.keys(people).length === 0) return [];
+  const filteredDirective = (people.length) ? people.filter(person => person.directorData) : people;
+  const directive = filteredDirective.map(director => {
+    return {...director, position: director.directorData.position}
+  });
+  const arraySort = ["President", "Vicepresident", "Secretari", "Tesorer", "Director", "DT", "Vocal"];
+  return sortByArray(directive, "position", arraySort);
 }
 
 const getPeople = state => get(state, "people.data");
