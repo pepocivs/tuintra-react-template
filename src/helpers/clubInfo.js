666ad4621835ce5117a7ed7c6f2b3189b6ad8fbd
Jorge Martín <tcbpepo@gmail.com>
2019-10-25 22:25:40 +0200

    feature: dynamic hostList

--- src/helpers/clubInfo.js
+++ src/helpers/clubInfo.js
@@ -1,4 +1,14 @@
-const subdomain = 'ginerdelosrios';
+/* eslint-disable no-restricted-globals */
+const getDomainEquivalences = () => {
+  const equivalences = {
+    'localhost': 'ginerdelosrios',
+    'ginerdelosrios.es': 'ginerdelosrios',
+    'inefhockey.com': 'inef',
+    'chmadrid.es': 'chmadrid'
+  };
+  return equivalences[location.hostname] || 'demo';
+}
+const subdomain = getDomainEquivalences();
 export default {
   apiHost: 'https://api.tuintra.com',
   subdomain,
