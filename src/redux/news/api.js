217b4fa39652c04764663fe085525146c7b80536
Jorge Martin <tcbpepo@gmail.com>
2020-09-04 01:31:01 +0200

    chore: add only visible news

--- src/redux/news/api.js
+++ src/redux/news/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import clubInfo from "helpers/clubInfo";
 
 const fetchNews = newSlug => {
-  let queryParams = '';
+  let queryParams = '?active=true';
   if (newSlug && newSlug.length > 0) queryParams = `?new=${newSlug}`;
   return axios.get(`${clubInfo.apiHost}/${clubInfo.subdomain}/news${queryParams}`);
 }
