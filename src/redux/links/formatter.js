221298b64d6f6f820d5ea1bfa7a91a2afd8bccb1
Jorge Martin <tcbpepo@gmail.com>
2021-12-08 13:11:17 +0100

    chore: adding new categories of partners to footer

--- src/redux/links/formatter.js
+++ src/redux/links/formatter.js
@@ -1,6 +1,6 @@
 export default (data) => {
   if (!data[0]) return [];
-  const footerCategories = [5, 6, 9, 10];
+  const footerCategories = [5, 6, 9, 10, 11, 12];
   const sortedLinks = sortByKey(data, 'categorySort');
   const links = sortedLinks.filter(link => (!footerCategories.includes(link.categoryId)) && link.links.length > 0);
   const footerLinks = sortedLinks.filter(link => footerCategories.includes(link.categoryId));
